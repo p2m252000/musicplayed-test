4ce7625ca9933bd703a102eaf4d1959da9a5c610
PhungQuocManh <1854050053Manh@ou.edu.vn>
2022-01-28 21:29:22 +0700

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,40 @@
+
+let allMusic = [
+  {
+    name: "Spectre",
+    artist: "Alan Walker",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Invincible",
+    artist: "DEAF KEV",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: " She Neva Knows",
+    artist: "Justatee",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Start (시작)",
+    artist: "Gaho (가호)",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Buâng khuâng",
+    artist: "Justatee",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Tình bạn vĩnh cửu",
+    artist: "Cindy V ",
+    img: "music-6",
+    src: "music-6"
+  },
+
+];
